4eb0ca932abe30096b2f91128110e239a92245a2
Zulfikar Jafar <polarbear2251@gmail.com>
2023-10-31 10:57:30 +0700

    //handling data kirim slip gaji

--- app/Http/Controllers/GajiController.php
+++ app/Http/Controllers/GajiController.php
@@ -98,65 +98,86 @@ class GajiController extends Controller
         if ($request->karyawan === 'all') {
             // dd('true');
             # code...
-            try {
-                $dataGaji = Gaji::where('bulan', $request->bulan)->where('tahun', $request->tahun)->get();
-                // dd($dataGaji);
-                foreach ($dataGaji as $key => $value) {
-                    // $explode = explode('_', $value->nik_pegawai);
-                    $data = Gaji::select('gaji.*', 'pegawai.*', 'jabatan.nama as nama_jabatan')
-                        ->join('pegawai', 'pegawai.nip_pegawai', '=', 'gaji.nik_pegawai')
-                        ->join('jabatan', 'jabatan.id', '=', 'pegawai.jabatan')
-                        ->where('gaji.nik_pegawai', $value->nik_pegawai)
-                        ->where('gaji.bulan', $value->bulan)
-                        ->first();
-                    // dd($data);
-                    // dd($data->gaji_pokok + $data->uang_makan + $data->uang_transport);
-                    // dd($data->status_pegawai);
-                    if ($data->status_pegawai == 1) {
-                        # code...
-                        $netto_gaji = HitungGaji::hitungTetap($value->nik_pegawai, $data->pinjaman, $data->adjustment, $data->supervisor, $value->bulan);
-                        $total_gaji = $data->uang_makan + $data->uang_transport + $data->gaji_pokok + $data->adjustment + $data->supervisor;
-                        $pph = 0;
-                    } else {
-                        $total_gaji = HitungGaji::hitungKontrak($value->nik_pegawai, $data->adjustment);
-                        $pph = HitungGaji::hitungPPH($value->nik_pegawai, $data->thr);
-                        $netto_gaji = abs($total_gaji - $pph);
-                        $total = $data->gaji_gross_kontrak + $data->adjustment;
-                        $total_gaji = $total - $data->pot_bjs_kes - $data->pot_bpjs_ket - $data->pot_jp;
-                    }
-                    // dd($data->pph);
-                    // dd($data);
-                    // dd($data->nama);
-                    $print = [
-                        'gaji' => $data,
-                        'netto' => $netto_gaji,
-                        'total_gaji' => $total_gaji,
-                        'pph' => $pph
-                    ];
 
-                    //code...
-                    $pdf = Pdf::loadView('gaji.cetak', $print)->setPaper('a4', 'portrait');
-                    $pdf->save(public_path('files/' . $value->nik_pegawai . '.pdf'));
-                    Mail::to($data->email)->send(new GajiMail($value->nik_pegawai, $data->nama));
-                    File::delete(public_path('files/' . $value->nik_pegawai . '.pdf'));
+
+            $dataGaji = Gaji::where('bulan', $request->bulan)->where('tahun', $request->tahun)->get();
+            if ($dataGaji->isEmpty()) {
+                return redirect()->back()->with('error', 'Data tidak ada!');
+            } else {
+                try {
+                    foreach ($dataGaji as $key => $value) {
+                        // dd('trues');
+                        // try {
+                        //code...
+
+                        $data = Gaji::select('gaji.*', 'pegawai.*', 'jabatan.nama as nama_jabatan')
+                            ->join('pegawai', 'pegawai.nip_pegawai', '=', 'gaji.nik_pegawai')
+                            ->join('jabatan', 'jabatan.id', '=', 'pegawai.jabatan')
+                            ->where('gaji.nik_pegawai', $value->nik_pegawai)
+                            ->where('gaji.bulan', $value->bulan)
+                            ->first();
+                        // dd($data);
+                        // } catch (\Throwable $th) {
+                        //     return redirect()->back()->with('error', 'Data Tidak Ada!');
+                        // }
+                        // $explode = explode('_', $value->nik_pegawai);
+                        // dd($data);
+                        // dd($data->gaji_pokok + $data->uang_makan + $data->uang_transport);
+                        // dd($data->status_pegawai);
+                        if ($data->status_pegawai == 1) {
+                            # code...
+                            $netto_gaji = HitungGaji::hitungTetap($value->nik_pegawai, $data->pinjaman, $data->adjustment, $data->supervisor, $value->bulan);
+                            $total_gaji = $data->uang_makan + $data->uang_transport + $data->gaji_pokok + $data->adjustment + $data->supervisor;
+                            $pph = 0;
+                        } else {
+                            $total_gaji = HitungGaji::hitungKontrak($value->nik_pegawai, $data->adjustment);
+                            $pph = HitungGaji::hitungPPH($value->nik_pegawai, $data->thr);
+                            $netto_gaji = abs($total_gaji - $pph);
+                            $total = $data->gaji_gross_kontrak + $data->adjustment;
+                            $total_gaji = $total - $data->pot_bjs_kes - $data->pot_bpjs_ket - $data->pot_jp;
+                        }
+                        // dd($data->pph);
+                        // dd($data);
+                        // dd($data->nama);
+                        $print = [
+                            'gaji' => $data,
+                            'netto' => $netto_gaji,
+                            'total_gaji' => $total_gaji,
+                            'pph' => $pph
+                        ];
+
+                        //code...
+                        $pdf = Pdf::loadView('gaji.cetak', $print)->setPaper('a4', 'portrait');
+                        $pdf->save(public_path('files/' . $value->nik_pegawai . '.pdf'));
+                        Mail::to($data->email)->send(new GajiMail($value->nik_pegawai, $data->nama));
+                        File::delete(public_path('files/' . $value->nik_pegawai . '.pdf'));
+                    }
+                    return redirect()->back()->with('success', 'Slip gaji berhasil terkirim');
+                } catch (\Throwable $th) {
+                    return redirect()->back()->with('error', $th->getMessage());
                 }
-                // dd($data);
-                return redirect()->back()->with('success', 'Slip gaji berhasil terkirim');
-            } catch (\Throwable $th) {
-                return redirect()->back()->with('error', $th->getMessage());
             }
+            // dd($dataGaji);
+
+            // dd($data);
+
         } else {
+            //code...
             $dataGaji = Gaji::where('bulan', $request->bulan)
                 ->where('tahun', $request->tahun)
                 ->where('nik_pegawai', $request->karyawan)
                 ->first();
-
-            $data = Gaji::select('gaji.*', 'pegawai.*', 'jabatan.nama as nama_jabatan')
-                ->join('pegawai', 'pegawai.nip_pegawai', '=', 'gaji.nik_pegawai')
-                ->join('jabatan', 'jabatan.id', '=', 'pegawai.jabatan')
-                ->where('gaji.nik_pegawai', $dataGaji->nik_pegawai)
-                ->where('gaji.bulan', $dataGaji->bulan)
-                ->first();
+            try {
+                //code...
+                $data = Gaji::select('gaji.*', 'pegawai.*', 'jabatan.nama as nama_jabatan')
+                    ->join('pegawai', 'pegawai.nip_pegawai', '=', 'gaji.nik_pegawai')
+                    ->join('jabatan', 'jabatan.id', '=', 'pegawai.jabatan')
+                    ->where('gaji.nik_pegawai', $dataGaji->nik_pegawai)
+                    ->where('gaji.bulan', $dataGaji->bulan)
+                    ->first();
+            } catch (\Throwable $th) {
+                return redirect()->back()->with('error', 'Data Tidak ada!');
+            }
             // dd($data->gaji_pokok + $data->uang_makan + $data->uang_transport);
             // dd($data->status_pegawai);
             if ($data->status_pegawai == 1) {
